1e77cafa606b07b3f61495c47a84fb42aa5e9012
Simon S. Soriano <simon0191@gmail.com>
2015-05-17 00:57:22 -0500

    Complete restart script

--- restart_unicorn.sh
+++ restart_unicorn.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+bundle install
+rake db:migrate
+rake assets:precompile
 unicorn_pid=`cat tmp/unicorn.pid`
 echo "Restarting Unicorn ($unicorn_pid)"
-kill -HUP $unicorn_pid
-
+kill $unicorn_pid
 bundle exec unicorn -E production -c ./config/unicorn/production.rb -D
 echo "Unicorn restarted"
