00731c4113e53f9f2e883a07dbfdfd90a0ca05a0
Simon S. Soriano <simon0191@gmail.com>
2015-05-17 05:01:38 -0500

    Implment theroku switcher

--- app/models/app.rb
+++ app/models/app.rb
@@ -14,13 +14,16 @@
 class App < ActiveRecord::Base
   SUBDOMAIN_REGEX = /\A[a-z]{2,}[a-z0-9-_]+\Z/i
 
+  THEROKU_SWITCHER_PATH = ENV["THEROKU_SWITCHER_PATH"] || "tmp/theroku_switcher.conf"
+  SITES_ENABLED_HALF_1 = ENV["SITES_ENABLED_HALF_1"] || "tmp/sites_enabled_half_1"
+  SITES_ENABLED_HALF_2 = ENV["SITES_ENABLED_HALF_2"] || "tmp/sites_enabled_half_2"
+
   validates :user, presence: true
   validates :url1, presence: true, format: {with: SUBDOMAIN_REGEX }
   validates :url2, presence: true, format: {with: SUBDOMAIN_REGEX }
   validates :subdomain, presence: true, uniqueness: true, format: {with: SUBDOMAIN_REGEX }
 
   belongs_to :user
-  after_validation :strip_herokudomain
   after_create :register_app
   after_update :update_app
   after_destroy do |app|
@@ -28,13 +31,20 @@ class App < ActiveRecord::Base
     reload_nginx
   end
 
-  APPS_AVAILABLE_DIR = ENV["APPS_AVAILABLE_DIR"] || "tmp/apps_available"
-  APPS_ENABLED_DIR = ENV["APPS_ENABLED_DIR"] || "tmp/apps_enabled"
-
-  def strip_herokudomain
-    self.url1.gsub!('.herokuapp.com','') if self.url1 != nil
-    self.url2.gsub!('.herokuapp.com','') if self.url2 != nil
-    create_nginx_config_file
+  def self.switch_apps
+    current = File.open(THEROKU_SWITCHER_PATH, &:readline)
+    if current.chomp.strip == "#1"
+      File.open(THEROKU_SWITCHER_PATH, 'w') do |f|
+        f.puts "#2"
+        f.puts "include #{SITES_ENABLED_HALF_2};"
+      end
+    else
+      File.open(THEROKU_SWITCHER_PATH, 'w') do |f|
+        f.puts "#1"
+        f.puts "include #{SITES_ENABLED_HALF_1};"
+      end
+    end
+    reload_nginx
   end
 
   private
@@ -53,11 +63,13 @@ class App < ActiveRecord::Base
   def delete_nginx_config_file(subdomain=nil)
     logger.info "Deleting * #{self.subdomain_was}.therokubalance.com *..."
     subdomain ||= self.subdomain
-    site_available = File.join(APPS_AVAILABLE_DIR,"#{subdomain}.therokubalance.com")
-    site_enabled = File.join(APPS_ENABLED_DIR,"#{subdomain}.therokubalance.com")
 
-    File.delete(site_available)
-    File.delete(site_enabled)
+    conf_file1 = site_conf_file(1,opts[:subdomain])
+    conf_file2 = site_conf_file(2,opts[:subdomain])
+
+    File.delete(config_file1)
+    File.delete(config_file2)
+
   end
 
   def create_nginx_config_file(opts={})
@@ -66,22 +78,34 @@ class App < ActiveRecord::Base
     opts[:url1] ||= self.url1
     opts[:url2] ||= self.url2
 
-    site_available = File.join(APPS_AVAILABLE_DIR,"#{opts[:subdomain]}.therokubalance.com")
-    site_enabled = File.join(APPS_ENABLED_DIR,"#{opts[:subdomain]}.therokubalance.com")
-    File.open(site_available, 'w') do |f|
-      f.puts %Q(
-server {
-  server_name #{opts[:subdomain]}.therokubalance.com;
-  location / {
-    proxy_pass  http://#{opts[:url1]}.herokuapp.com;
-  }
-}
-      )
-    end
-    system "ln -s #{site_available} #{site_enabled}"
+    conf_file1 = site_conf_file(1,opts[:subdomain])
+    conf_file2 = site_conf_file(2,opts[:subdomain])
+
+    File.open(conf_file1, 'w') { |f| f.puts subdomain_conf(url: opts[:url1]) }
+    File.open(conf_file2, 'w') { |f| f.puts subdomain_conf(url: opts[:url2]) }
   end
 
   def reload_nginx
     system "nginx -s reload"
+  end
+
+  def site_conf_file(half,subdomain)
+    File.join(
+      [SITES_ENABLED_HALF_1,SITES_ENABLED_HALF_2][half-1],
+      "#{subdomain}.therokubalance.com"
+    )
+  end
+
+  def subdomain_conf(opts={})
+    opts[:subdomain] ||= self.subdomain
+    opts[:url] ||= self.url1
+    %Q(
+server {
+  server_name #{opts[:subdomain]}.therokubalance.com;
+  location / {
+    proxy_pass  http://#{opts[:url]}.herokuapp.com;
+  }
+}
+    )
   end
 end
