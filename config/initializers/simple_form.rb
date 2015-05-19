2fc728666218216f19349378e3342ed1e1eaca46
Simon S. Soriano <simon0191@gmail.com>
2015-05-18 23:52:39 -0500

    Fix simple form deprecated method call

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -52,7 +52,7 @@ SimpleForm.setup do |config|
   end
 
   # The default wrapper to be used by the FormBuilder.
-  config.form_class = 'simple_form form-horizontal'
+  config.default_form_class = 'simple_form form-horizontal'
   config.default_wrapper = :default
 
   # Define the way to render check boxes / radio buttons with labels.
